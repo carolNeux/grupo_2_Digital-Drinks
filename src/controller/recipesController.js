97650f1433e6e15bd67e5b38b1dd32af66f241f8
Carlos-Loffreda <carlosloffmil987@gmail.com>
2020-11-02 01:32:01 -0300

    se modifico recipescontroller con logica de creacion de recetas

--- src/controller/recipesController.js
+++ src/controller/recipesController.js
@@ -4,31 +4,46 @@ const {update} = require('./productsController');
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const recipesFilePath = path.join(__dirname, '../data/recipesDB.json');
-const recipes = JSON.parse(fs.readFileSync(recipesFilePath, {encoding: 'utf-8'}));
+const getRecipes = ()=> JSON.parse(fs.readFileSync(recipesFilePath, {encoding: 'utf-8'}));
 
 module.exports = {
     index : (req,res) => {
+        let recipes = getRecipes();
         res.render('./recipes/recipes', {recipes})
     },
     detail: (req,res) => {
+        let recipes = getRecipes();
         let idDetail = req.params.id;
 		let recipeDetail = recipes.find(recipe=> 
 			recipe.id == idDetail); 
-        res.render('./recipes/recipesDetail', {'recipeDetail': recipeDetail, toThousand});
+        res.render('./recipes/recipesDetail', {'recipeDetail': recipeDetail});
     },
     new:  (req,res) => {
-        res.render('./recipes/recipes-create-form') // decidir si se crea un formulario ????
+        let recipes = getRecipes();
+        res.render('./recipes/recipes-create-form')
     },
     create: (req, res, next) => {
+        let recipes = getRecipes();
+        let newRecipe = {
+            id: recipes[recipes.length -1].id + 1,
+            ...req.body,
+            image: req.file.filename,
+        };
+        let newDb = JSON.stringify(
+            [...recipes, newRecipe], null, 2
+        );
+        fs.writeFileSync(recipesFilePath, newDb);
         res.redirect('/recipes');
     },
     edit:  (req,res) => {
+        let recipes = getRecipes();
         let idDetail = req.params.id;
 		let recipeDetail = recipes.find(recipe=> 
 			recipe.id == idDetail); 
         res.render('./recipes/recipes-edit-form.ejs', {'recipeDetail': recipeDetail, toThousand}) // Buscar y enviar el producto a editar a la vista
     },
     update: (req, res, next) => {
+        let recipes = getRecipes();
         res.render('/recipes')
     }
 }
