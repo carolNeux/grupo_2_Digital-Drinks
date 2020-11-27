446d248ac992c3e53aa0c0a7803733b26e1cae0e
robrondon <robrondon11@gmail.com>
2020-11-27 12:16:19 -0300

    actualizo db products e instalo sequelize

---
+++ src/database/config/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "root",
+    "database": "digitaldrinks_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    define: {
+      underscored: true,
+      paranoid: true
+    },
+    "port": "8889"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
