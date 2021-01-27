b5ee30be1bd281a372e27f3e86e79e1297a2d1a0
xayjo <xayjo.xecv@gmail.com>
2021-01-27 00:21:31 -0300

    arreglo de css y middleware

--- src/middlewares/routes/allowed.js
+++ src/middlewares/routes/allowed.js
@@ -1,9 +1,9 @@
 module.exports = function (req,res,next) {
-    if (req.session.userCategory === 1) {
+    if (req.session.userCategory === 1 || req.session.userCategory === 2) {
         next();
-    } else if (req.session.userCategory === 2) {
-        res.render('./users/forbidden');
-    }
+    }// else if (req.session.userCategory === 2) {
+    //     res.render('./users/forbidden');
+    // }
     else {
         res.redirect('/users/login');
     }   
